af5a2776afbc450664f310f4095711dc2bf1547d
Lurrégia Barreto <luhhbarreto@gmail.com>
2021-10-08 19:40:51 -0300

    Exibir vendas

--- src/br/com/zup/ServicoVenda.java
+++ src/br/com/zup/ServicoVenda.java
@@ -7,10 +7,18 @@ public class ServicoVenda {
     private static List<Venda> vendas = new ArrayList<>();
 
     //Cadastro vendas
-    public static Venda cadastrarVenda(String nomeDoProduto, double valor, String dataDeRegistro, String email, String cpf){
+    public static Venda cadastrarVenda(String nomeDoProduto, double valor, String dataDeRegistro, String email, String cpf) {
         Venda venda = new Venda();
         vendas.add(venda);
         return venda;
+    }
+    //Exibir vendas
+
+    public static void ExibirVendas() {
+        for (Venda refencia : vendas) {
+            System.out.println(refencia);
+        }
+
     }
 
 }
