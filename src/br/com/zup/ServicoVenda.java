1a47c9830b435ebff602a9922f8525a9a679be43
Lurrégia Barreto <luhhbarreto@gmail.com>
2021-10-08 19:39:30 -0300

    cadastrar vendas

--- src/br/com/zup/ServicoVenda.java
+++ src/br/com/zup/ServicoVenda.java
@@ -5,4 +5,12 @@ import java.util.List;
 
 public class ServicoVenda {
     private static List<Venda> vendas = new ArrayList<>();
+
+    //Cadastro vendas
+    public static Venda cadastrarVenda(String nomeDoProduto, double valor, String dataDeRegistro, String email, String cpf){
+        Venda venda = new Venda();
+        vendas.add(venda);
+        return venda;
+    }
+
 }
