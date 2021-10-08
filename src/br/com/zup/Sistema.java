1113a74345ed0b776521083acc0844b308faea95
Lurrégia Barreto <luhhbarreto@gmail.com>
2021-10-08 18:28:24 -0300

    realizar menu para o usuário

--- src/br/com/zup/Sistema.java
+++ src/br/com/zup/Sistema.java
@@ -8,4 +8,20 @@ public class Sistema {
         System.out.println(mensagem);
         return new Scanner(System.in);
     }
+
+    //Menu ofericido ao usuário
+    public static void menu(){
+        System.out.println("Digite [1]  Cadastrar um cliente");
+        System.out.println("Digite [2]  Cadastrar um vendedor");
+        System.out.println("Digite [3]  Cadastrar Venda");
+        System.out.println("Digite [4]  Exibir vendas");
+        System.out.println("Digite [5]  Exibir clientes");
+        System.out.println("Digite [6]  Exibir vendedores");
+        System.out.println("Digite [7]  Exibir as compras de um cliente:");
+        System.out.println("Digite [8]  Exibir as vendas de um vendedor:");
+        System.out.println("Digite [9]  Sair do programa");
+    }
+
+
 }
+
