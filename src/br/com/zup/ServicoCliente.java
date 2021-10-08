1c9227581746508e5db0d6c4788f7017c58872c3
Lurrégia Barreto <luhhbarreto@gmail.com>
2021-10-08 18:34:23 -0300

    criar método cadastrar cliente

--- src/br/com/zup/ServicoCliente.java
+++ src/br/com/zup/ServicoCliente.java
@@ -6,4 +6,10 @@ import java.util.List;
 public class ServicoCliente {
     private static List<Cliente> clientes = new ArrayList<>();
 
+    public static Cliente cadastrarCliente(String nome, String cpf, String email) {
+        Cliente cliente = new Cliente(nome, cpf, email);
+        clientes.add(cliente);
+        return cliente;
+    }
+
 }
