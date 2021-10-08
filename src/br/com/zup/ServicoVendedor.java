612fede8a88bcaabb6a80d088863f6ff97d51fdf
Lurrégia Barreto <luhhbarreto@gmail.com>
2021-10-08 18:35:23 -0300

    criar método para cadastrar vendedor

--- src/br/com/zup/ServicoVendedor.java
+++ src/br/com/zup/ServicoVendedor.java
@@ -5,4 +5,9 @@ import java.util.List;
 
 public class ServicoVendedor {
     private static List<Vendedor> vendedores = new ArrayList<>();
+    public static Vendedor cadastrarVendedor(String nome, String cpf, String email){
+        Vendedor vendedor = new Vendedor(nome, cpf, email);
+        vendedores.add(vendedor);
+        return vendedor;
+    }
 }
