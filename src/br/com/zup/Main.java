6cacdf62bbe1812a950466eef9f7cedf56e1fbcc
Lurrégia Barreto <luhhbarreto@gmail.com>
2021-10-09 15:57:02 -0300

    executar main

--- src/br/com/zup/Main.java
+++ src/br/com/zup/Main.java
@@ -3,6 +3,13 @@ package br.com.zup;
 public class Main {
 
     public static void main(String[] args) {
-
+        boolean Menu = true;
+        while (Menu){
+            try {
+                Menu = Sistema.executar();
+            }catch (Exception erro){
+                System.out.println(erro.getMessage());
+            }
+        }
     }
 }
