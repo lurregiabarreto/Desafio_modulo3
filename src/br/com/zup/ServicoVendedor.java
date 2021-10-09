71c39ea74ec32898d7ad3cdc1e91eb14d9fcb9d4
Lurrégia Barreto <luhhbarreto@gmail.com>
2021-10-09 15:18:20 -0300

    acionar lista de vendedores

--- src/br/com/zup/ServicoVendedor.java
+++ src/br/com/zup/ServicoVendedor.java
@@ -5,19 +5,40 @@ import java.util.List;
 
 public class ServicoVendedor {
     private static List<Vendedor> vendedores = new ArrayList<>();
-    public static Vendedor cadastrarVendedor(String nome, String cpf, String email){
+
+    public static Vendedor cadastrarVendedor(String nome, String cpf, String email) {
         Vendedor vendedor = new Vendedor(nome, cpf, email);
         vendedores.add(vendedor);
         return vendedor;
     }
 
     //Verificar  e-mail já castrado
-    public static void verificarEmail(String email) throws Exception{
-        for (Vendedor referencia : vendedores){
-            if (referencia.getEmail().equals(email)){
+    public static void verificarEmail(String email) throws Exception {
+        for (Vendedor referencia : vendedores) {
+            if (referencia.getEmail().equals(email)) {
                 throw new Exception("Esse e-mail já está cadastrado");
             }
         }
     }
 
+
+   //buscar vendedores pelo email
+    public static Vendedor buscarVendedor(String email) throws Exception {
+        for (Vendedor referencia : vendedores) {
+            if (referencia.getEmail().equals(email)) {
+                return referencia;
+            }
+        }
+        throw new Exception("Nenhum email cadastrado");
+
+    }
+    //Exibir de vendedores para usuario
+    public static void mostrarListaVendedores(){
+        for(Vendedor referencia : vendedores){
+            System.out.println(referencia);
+        }
+    }
+
+
 }
+
